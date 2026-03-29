650bea3b4634302403b6fa4b6579003635ea12f8
DmitryLoginov <v94-gb4h4@yandex.ru>
2026-03-29 16:58:27 +0300

    fix(core): добавить клиенты в @EnableFeignClients и указать пакеты в @ComponentScan

--- core/comment-service/src/main/java/ru/practicum/core/comment/CommentApplication.java
+++ core/comment-service/src/main/java/ru/practicum/core/comment/CommentApplication.java
@@ -4,8 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
+import org.springframework.context.annotation.ComponentScan;
+import ru.practicum.core.common.api.client.EventClient;
+import ru.practicum.core.common.api.client.UserClient;
+
+@EnableFeignClients(clients = {EventClient.class, UserClient.class})
 @SpringBootApplication
+@ComponentScan(basePackages = {"ru.practicum.core.comment", "ru.practicum.core.common.api.fallback"})
 public class CommentApplication {
     public static void main(String[] args) {
         SpringApplication.run(CommentApplication.class, args);
--- core/event-service/src/main/java/ru/practicum/core/event/EventApplication.java
+++ core/event-service/src/main/java/ru/practicum/core/event/EventApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
-@EnableFeignClients
+import ru.practicum.core.common.api.client.CommentClient;
+import ru.practicum.core.common.api.client.RequestClient;
+import ru.practicum.core.common.api.client.UserClient;
+
+@EnableFeignClients(clients = {UserClient.class, CommentClient.class, RequestClient.class})
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@ComponentScan(basePackages = {"ru.practicum.core.event", "ru.practicum.core.common.api.fallback"})
 public class EventApplication {
     public static void main(String[] args) {
         SpringApplication.run(EventApplication.class, args);
