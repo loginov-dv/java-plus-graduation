990bc767a12b50ba6d17cbc259e9cb1901b8f771
DmitryLoginov <v94-gb4h4@yandex.ru>
2026-03-30 22:22:59 +0300

    fix(core.common): удалить неиспользуемый метод в EventApiContract

--- core/common/src/main/java/ru/practicum/core/common/api/contract/EventApiContract.java
+++ core/common/src/main/java/ru/practicum/core/common/api/contract/EventApiContract.java
@@ -1,6 +1,5 @@
 package ru.practicum.core.common.api.contract;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -8,10 +7,6 @@ import ru.practicum.core.common.dto.event.EventFullDto;
 
 // контракт для внутненнего API
 public interface EventApiContract {
-    @GetMapping("/events/{eventId}")
-    EventFullDto getEvent(@PathVariable Long eventId,
-                          HttpServletRequest request);
-
     @GetMapping("/events/inner/{eventId}")
     EventFullDto getEventInner(@PathVariable Long eventId);
 }
