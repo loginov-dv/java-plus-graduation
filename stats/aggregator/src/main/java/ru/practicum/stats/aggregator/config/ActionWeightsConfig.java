73e01be3c03b4f234f39669b550a49a21a5a5ef8
DmitryLoginov <v94-gb4h4@yandex.ru>
2026-04-04 18:59:46 +0300

    feat(infra.config-server): добавить конфигурационные классы KafkaConfig и ActionWeightsConfig

---
+++ stats/aggregator/src/main/java/ru/practicum/stats/aggregator/config/ActionWeightsConfig.java
@@ -0,0 +1,15 @@
+package ru.practicum.stats.aggregator.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@Getter
+@AllArgsConstructor
+@ConfigurationProperties("action-weights")
+public class ActionWeightsConfig {
+    private double view;
+    private double register;
+    private double like;
+}
---
+++ stats/aggregator/src/main/java/ru/practicum/stats/aggregator/config/KafkaConfig.java
@@ -0,0 +1,30 @@
+package ru.practicum.stats.aggregator.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@Getter
+@AllArgsConstructor
+@ConfigurationProperties("kafka")
+public class KafkaConfig {
+    private String server;
+    private Topics topics;
+    private Consumer consumer;
+
+    @Getter
+    @AllArgsConstructor
+    public static class Topics {
+        private String users;
+        private String events;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Consumer {
+        private String clientId;
+        private String groupId;
+        private Integer pollDurationMs;
+    }
+}
