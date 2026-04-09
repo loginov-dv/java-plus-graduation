0ca5952c4358322b547668efc1c1ee8bf76ac372
DmitryLoginov <v94-gb4h4@yandex.ru>
2026-04-09 20:29:59 +0300

    feat(stats.analyzer): добавить репозитории

---
+++ stats/analyzer/src/main/java/ru/practicum/stats/analyzer/repository/EventSimilarityRepository.java
@@ -0,0 +1,23 @@
+package ru.practicum.stats.analyzer.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.jpa.repository.Query;
+import ru.practicum.stats.analyzer.model.EventSimilarity;
+import ru.practicum.stats.analyzer.model.EventSimilarityId;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+public interface EventSimilarityRepository extends JpaRepository<EventSimilarity, EventSimilarityId> {
+    Optional<EventSimilarity> findByEventAAndEventB(long eventA, long eventB);
+
+    @Query("SELECT es FROM EventSimilarity es " +
+            "WHERE es.eventA = :eventId OR es.eventB = :eventId")
+    List<EventSimilarity> findByEventAOrEventB(long eventId);
+
+    @Query("SELECT es FROM EventSimilarity es " +
+            "WHERE es.eventA IN :eventIds OR es.eventB IN :eventIds")
+    List<EventSimilarity> findByEventAInOrEventBIn(Collection<Long> eventIds);
+}
